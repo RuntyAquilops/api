09934ca1f0e9abade783f66e9c7861e672cc8ea0
kslow <kslow@student.21-school.ru>
2021-01-12 02:24:01 +0300

    fix sort

--- rooms/sort.php
+++ rooms/sort.php
@@ -1,12 +1,22 @@
 <?php
 
+include_once 'error.php';
+
 function getSortRooms($connection, $data)
 {
-    $query = $connection->query("SELECT * FROM room_list ORDER BY $data");
-    $rooms_list = [];
-    while($room = $query->fetch_assoc()) {
-        $rooms_list[] = $room;
+    $er = new Errors();
+
+    // echo strstr($data, "cost_per_night");
+    if (strstr($data, "cost_per_night") != false || strstr($data, "data_create") != false) {
+        $query = $connection->query("SELECT * FROM room_list ORDER BY $data");
+        $rooms_list = [];
+        while($room = $query->fetch_assoc()) {
+            $rooms_list[] = $room;
+        }
+        echo json_encode($rooms_list, JSON_PRETTY_PRINT);
     }
-    echo json_encode($rooms_list, JSON_PRETTY_PRINT);
+    else {
+        $er->err("Uncorrect parameter");
+    }
 }
 ?>
